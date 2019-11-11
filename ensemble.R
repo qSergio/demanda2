15650092b7f878a196295764770663e2443d86c3
qSergio <ssnieto@ciencias.unam.mx>
2019-11-11 00:21:36 -0600

    Se agregan desempeños y gráficas

--- ensemble.R
+++ ensemble.R
@@ -14,7 +14,7 @@ acti <- read.csv("training_data_d.csv")
 #Esto es con datos de supertienda
 activ <- log(retail$cantidad)
 
-
+prueba <- activ[53:208] %>% ts(frequency = 52)
 
 train <- activ[53:156] %>% ts(frequency = 52)
 test <- activ[157:208] %>% ts(frequency = 52)
@@ -33,13 +33,13 @@ tt <- cbind(c(1:(length(train) + h), rep(0, h)))
 
 # Fit a network with no differencing, no univariate lags, and fixed deterministic trend: 
 my_mlp <- mlp(train, 
-              hd = 50, 
+              hd = 10, 
               difforder = c(0,1), 
               lags = 1, 
               xreg = tt, 
               xreg.lags = list(0), 
               outplot=TRUE,
-              reps = 10,
+              reps = 6,
               xreg.keep = TRUE)
 
 # Make predictions: 
@@ -47,7 +47,7 @@ pred <- forecast(my_mlp, h = h, xreg = tt)
 
 # Data Frame for comparing: 
 
-df_mlp <- data_frame(Actual = test %>% as.vector(), 
+df_mlp <- tibble(Actual = test %>% as.vector(), 
                      Predicted = pred$mean, 
                      Error = Predicted - Actual,
                      Error_Percent = Error / Actual)#round(Error / Actual, 2))
@@ -81,14 +81,14 @@ get_accuracy_measures <- function(your_result_df) {
 #  Auto ARIMA Approach as proposed by Hyndman and Khandakar (2008)
 #==================================================================
 
-my_arima <- auto.arima(train,D=1)
-
+my_arima <- auto.arima(train,d=1,D=1)
+summary(my_arima)
 # Use the model for forecasting: 
 predicted_arima <- forecast(my_arima, h = h)$mean %>% as.vector()
 
 
 # Data Frame for comparing: 
-df_arima <- data_frame(Actual = test %>% as.vector(), 
+df_arima <- tibble(Actual = test %>% as.vector(), 
                        Predicted = predicted_arima, 
                        Error = Predicted - Actual,
                        Error_Percent = Error / Actual)#round(Error / Actual, 2))
@@ -96,6 +96,15 @@ df_arima <- data_frame(Actual = test %>% as.vector(),
 # Model Performance: 
 df_arima %>% knitr::kable()
 
+
+out.yp[,1]
+df_bugs <- tibble(Actual = test %>% as.vector(), 
+                   Predicted = out.yp[,1], 
+                   Error = Predicted - Actual,
+                   Error_Percent = Error / Actual)#round(Error / Actual, 2))
+
+# Model Performance: 
+df_bugs %>% knitr::kable()
 
 #=======================================
 #    My Solution: Ensemble Method 
@@ -106,30 +115,30 @@ my_ensemble_predict <- function(your_df, n_ahead) {
   N <- length(your_df)
   end1 <- N - n_ahead
   
-  train <- your_df[1:end1] %>% ts(frequency = 14)
-  test <- your_df[(end1 + 1):N] %>% ts(frequency = 14)
+  train <- your_df[1:end1] %>% ts(frequency = 52)
+  test <- your_df[(end1 + 1):N] %>% ts(frequency = 52)
   h <- length(test)
   
   tt <- cbind(c(1:(length(train) + h), rep(0, h)))
   
   my_mlp <- mlp(train, 
-                hd = 30, 
+                hd = 10, 
                 difforder = c(0,1), 
-                lags = 1, 
+                lags = 2, 
                 xreg = tt, 
                 xreg.lags = list(0), 
                 outplot=TRUE,
-                reps = 5,
+                reps = 10,
                 xreg.keep = TRUE)
   
   predicted_mlp <- forecast(my_mlp, h = h, xreg = tt)
   
-  my_arima <- auto.arima(train,D=1)
+  my_arima <- auto.arima(train,d=1,D=1)
   predicted_arima <- forecast(my_arima, h = h)$mean %>% as.vector()
   
-  ensemble_predicted <- 0.5*(predicted_mlp$mean + predicted_arima)
+  ensemble_predicted <- 0.25*(predicted_mlp$mean) + 0.75*(predicted_arima)
   
-  return(data_frame(Actual = test %>% as.vector(), 
+  return(tibble(Actual = test %>% as.vector(), 
                     Predicted = ensemble_predicted, 
                     Error = Predicted - Actual,
                     Error_Percent = Error / Actual) )#)round(Error / Actual, 2)))
@@ -137,7 +146,7 @@ my_ensemble_predict <- function(your_df, n_ahead) {
 
 
 # Use the function: 
-my_ensemble_predict(activ, 26) -> df_ensemble
+my_ensemble_predict(prueba, 52) -> df_ensemble #usar prueba
 df_ensemble %>% knitr::kable()
 
 
@@ -145,10 +154,13 @@ df_ensemble %>% knitr::kable()
 
 bind_rows(df_mlp %>% get_accuracy_measures(), 
           df_arima %>% get_accuracy_measures(), 
-          df_ensemble %>% get_accuracy_measures()) %>% 
-  mutate(Approach = c("MLP", "ARIMA", "Ensemble")) %>% 
+          df_ensemble %>% get_accuracy_measures(),
+          df_bugs %>% get_accuracy_measures()) %>% 
+  mutate(Approach = c("MLP", "ARIMA", "Ensemble", "Modelo Estacional")) %>% 
   select(Approach, everything()) %>% 
   mutate_if(is.numeric, function(x) {round(x, 2)}) %>% 
   knitr::kable()
+
+## Ya contiene Bugs y es el mejor en estas métricas
 
 df_mlp %>% get_accuracy_measures()
